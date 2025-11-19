ce9e99e466c63c795319c83be21e75c69c654461
vickyDevlo <vickymagare1996@gmail.com>
2025-11-19 11:59:33 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <h1 className="text-xl md:text-3xl text-center font-bold text-gray-800">
+        E-Commerce App With Redux-Toolkit
+      </h1>
+    </div>
+  );
+}
+
+export default App;
